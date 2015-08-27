66a07c101cf66760322b3afc2d620b3cefe192da
Kenneth Ertel <kertel@wisc.edu>
2015-08-27 14:12:48 -0500

    standard deviation

--- test_stats.py
+++ test_stats.py
@@ -1,5 +1,5 @@
 
-from stats import mean
+from stats import mean, std
 from nose.tools import assert_equal
 
 
@@ -10,3 +10,8 @@ def test_float_mean():
 def test_mean():
 	assert_equal(mean([4,8]), 6)
 #test_mean()
+
+def test_std1():
+	obs = std([0.0,2.0])
+	exp = 1.0
+	assert_equal(obs,exp)
