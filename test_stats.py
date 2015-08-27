4638e5c14b198d4440fab10d1ddf663b816d9e6b
Kenneth Ertel <kertel@wisc.edu>
2015-08-27 13:40:18 -0500

    nose testing

--- test_stats.py
+++ test_stats.py
@@ -1,14 +1,16 @@
 
 from stats import mean
+from nose.tools import assert_equal
+
 
 def test_mean():
-	assert(mean([2,4]) == 3)
-test_mean()
+	assert_equal(mean([2,4]), 3)
+#test_mean()
 
 def test_float_mean():
 	assert(mean([1,2]) == 1.5)
-test_float_mean()
+#test_float_mean()
 
 def test_mean():
 	assert(mean([4,8]) == 6)
-test_mean()
+#test_mean()
